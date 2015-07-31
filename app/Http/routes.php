62175453f92924f754c0989612390fa52093d9ed
Sébastien Muller <igniweb.net@gmail.com>
2015-08-01 00:26:44 +0200

    Still playing a bit w/ Lumen (Logger)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,18 @@
 <?php
 
 $app->get('/', function () use ($app) {
-    throw new App\Exceptions\AuthException('Invalid user credentials.');
+    //dd($app);
+    //dd(app()->environment());
+
+    //app('log')->debug('Debug');
+    //app('log')->info('Info');
+    //app('log')->notice('Notice');
+    //app('log')->warning('Warning');
+    //app('log')->error('Error');
+    //app('log')->critical('Critical');
+    //app('log')->alert('Alert');
     
+    //throw new App\Exceptions\AuthException('Invalid user credentials.');
+
     return $app->welcome();
 });
